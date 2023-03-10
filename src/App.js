d33d384316f83fb06148b598a246d550efe729ff
Sandip3sp <sandippol40@gmail.com>
2023-03-10 22:28:57 +0530

    react day 4 assignment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./card.css";
+import CardProfile from "./card";
 
 function App() {
+  const User1 = {
+    name: "Amit",
+    image: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg",
+    designation: "Graphic Designer ",
+    description:
+      "Highly creative and multitalented Graphic Designer with extensive experience in multimedia, marketing, and print design.",
+  };
+
+  const User2 = {
+    name: "Ruhi",
+    image:
+      "https://images.pexels.com/photos/3775131/pexels-photo-3775131.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    description:
+      "perform a variety of music for recordings and live audiences. They audition for positions in choruses, orchestras, bands, plays, 				and other types of music groups.",
+    designation: "Singer",
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="inline">
+          <CardProfile user={User1} />
+          <br></br>
+
+          <CardProfile user={User2} />
+        </div>
       </header>
     </div>
   );
