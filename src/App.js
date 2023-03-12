d07885a6ab547ae0cf9b04c3e208e7b48ffda6cb
Sandip3sp <sandippol40@gmail.com>
2023-03-13 01:13:11 +0530

    react day 5 assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Fragment } from "react";
+import "./App.css";
+
+import Buttons from "./Buttons";
+import Names from "./Names";
+import { Todos } from "./Todos";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Todos></Todos>
+
+      <br />
+      <br />
+
+      <Buttons></Buttons>
+
+      <br />
+      <br />
+
+      <Names></Names>
+    </>
   );
 }
 
