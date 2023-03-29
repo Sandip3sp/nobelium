efa070bf9abab7e4ea64b0f1e8685f8ce545d16f
Sandip3sp <sandippol40@gmail.com>
2023-03-30 01:41:06 +0530

    Guess No assignment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GenerateNo from "./Random";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Guess">
+      <GenerateNo />
+    </div>
+  );
+}
+
+export default App;
